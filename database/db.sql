3d781706e97f1fa6fd3926c8d8902f8e188c9806
Marcos Aguado <marcos1903@live.com.ar>
2022-02-07 20:46:14 -0300

    script create database

--- database/db.sql
+++ database/db.sql
@@ -3,9 +3,30 @@ CREATE DATABASE alkemy_fullstack;
 USE alkemy_fullstack;
 
 CREATE TABLE user(
-  id INT NOT NULL,
-  email VARCHAR(30) NOT NULL,
-  username VARCHAR(18) NOT NULL,
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  username VARCHAR(20) NOT NULL,
   password VARCHAR(50) NOT NULL,
+  email VARCHAR(30) NOT NULL,
+  firstname VARCHAR(40) NOT NULL,
+  lastname VARCHAR(40) NOT NULL,
+);
+
+CREATE TABLE transaction(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  concept VARCHAR(20) NOT NULL,
+  amount DOUBLE(9,2) NOT NULL,
+  date VARCHAR(10) NOT NULL,
+  user_id INT NOT NULL,
+  id_type_transaction INT NOT NULL,
+);
+
+CREATE TABLE category(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  name VARCHAR(20) NOT NULL,
+);
+
+CREATE TABLE type_transaction(
+  id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+  type VARCHAR(20) NOT NULL,
 );
 
