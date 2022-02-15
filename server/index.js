5efc20d4862a7a6f111e1a8a15c47037ec386ee4
Marcos Aguado <marcos1903@live.com.ar>
2022-02-15 09:21:41 -0300

    ABM y GETS terminados, faltan gets por ID.

--- server/index.js
+++ server/index.js
@@ -47,8 +47,13 @@ app.get('/', (req, res) => {
   res.send('<h1>Hello World from root route</h1>');
 });
 
+// GLOBALES
 
-// AGREGAR UNA TRANSACCION
+const is_deleted = 'Y';
+const is_not_deleted = 'N';
+
+
+// AGREGAR TRANSACCION
 app.post('/transaction/add', async (req, res) => {
   const { concept, amount, date, user_id, id_type_transaction } = req.body;
   const newTransaction = {
@@ -56,20 +61,45 @@ app.post('/transaction/add', async (req, res) => {
     amount,
     date,
     user_id,
-    id_type_transaction
+    id_type_transaction,
+    is_not_deleted
   };
 
-  await db.query('INSERT INTO transaction set ?', [newTransaction]);
+  await db.query('INSERT INTO transaction SET ?', [newTransaction]);
   console.log(newTransaction);
   res.send('request received');
 });
 
+  
 
 // OBTENER TRANSACCIONES
 app.get('/transaction/all', async (req, res) => {
-  const transactions = await db.query('SELECT id, concept, amount, date, user_id, id_type_transaction FROM transaction;');
+
+  const transactions = await db.query('SELECT id, concept, amount, date, user_id, id_type_transaction, is_deleted FROM transaction WHERE is_deleted=?;', is_not_deleted);
   console.log(transactions);
-  res.send('transactions iran aca');
+  res.send(transactions);
+
+});
+
+// EIDTAR TRANSACCION
+app.put('/transaction/update', async (req, res) => {
+  const { id, concept, amount, date } = req.body;
+
+  // eslint-disable-next-line quotes
+  await db.query(`UPDATE transaction SET concept=${concept}, amount=${amount}, date=${date} WHERE id=${id};`);
+  res.send('transaction updated');
+});
+
+//ELIMINAR TRANSACCION
+app.delete('/transaction/delete', async (req, res) => {
+  const { id } = req.body;
+
+  // eslint-disable-next-line quotes
+  const transactionDeleted =  await db.query(`UPDATE transaction SET is_deleted=? WHERE id=${id}`,is_deleted);
+
+  console.log(transactionDeleted);
+  res.send('transaction deleted');
+
 
 });
 
@@ -78,32 +108,69 @@ app.get('/transaction/all', async (req, res) => {
 app.post('/user/add', async (req, res) => {
   const { username, password, email, firstname, lastname } = req.body;
 
-  const newUser = { username, password, email, firstname, lastname };
+  const newUser = { username, password, email, firstname, lastname, is_not_deleted };
 
   await db.query('INSERT INTO user SET ?', [newUser]);
   console.log(newUser);
   res.send('user added');
 });
 
+// EDITAR USUARIO
 app.put('/user/update', async (req, res) => {
   const { id, password, email, firstname, lastname } = req.body;
-
-  //const modifyUser = { id, password, email, firstname, lastname };
 
   // eslint-disable-next-line quotes
   await db.query(`UPDATE user SET password=${password}, email=${email}, firstname=${firstname}, lastname=${lastname} WHERE id=${id};`);
   res.send('user updated');
 });
 
+// ELIMINAR USUARIO
 app.delete('/user/delete', async (req, res) => {
   const { id } = req.body;
 
-  await db.query(`UPDATE user SET is_deleted=${'Y'} WHERE id=${id};`);
+  await db.query(`UPDATE user SET is_deleted=${is_deleted} WHERE id=${id};`);
 
   res.send('user deleted');
 
 });
 
+// AGREGAR CATEGORIA
+
+app.post('/category/add', async (req, res) => {
+  const { name } = req.body;
+
+  const newCategory = {name, is_not_deleted};
+  // eslint-disable-next-line quotes
+  await db.query(`INSERT INTO category SET ?`, [newCategory]);
+  res.send('category added');
+
+});
+
+// OBTENER TRANSACCIONES
+app.get('/category/all', async (req, res) => {
+  const categories = await db.query(`SELECT id, name, is_deleted FROM category WHERE is_deleted=${is_not_deleted}`);
+  console.log(categories);
+  res.send(categories);
+});
+
+// EDITAR TRANSACCION
+
+app.put('/category/update', async (req, res) => {
+  const { id, name } = req.body;
+  
+  // eslint-disable-next-line quotes
+  await db.query(`UPDATE category SET name=${name} WHERE id=${id}`);
+  
+  res.send('category edited');
+
+});
+
+app.delete('/category/delete', async (req, res) => {
+  const { id } = req.body;
+
+  await db.query(`UPDATE category SET is_deleted=${is_deleted} WHERE id=${id}`);
+  res.send('category deleted');
+}); 
 
 app.listen(PORT, () => {
   console.log(`Server running on localhost:${PORT}`);
