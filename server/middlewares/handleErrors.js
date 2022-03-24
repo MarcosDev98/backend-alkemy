99da1a96051ae70fa4b858c66668a510da98dbe7
Marcos Aguado <marcos1903@live.com.ar>
2022-03-24 12:38:39 -0300

    Se agrego un nuevo error al diccionario de errores.

--- server/middlewares/handleErrors.js
+++ server/middlewares/handleErrors.js
@@ -5,6 +5,7 @@ const ERRORS_HANDLERS = {
   JsonWebTokenError: (response) => response.status(401).json({ error: 'token missing or invalid' }),
   DefaultError: (response) => response.status(500).end(),
   TokenExpiredError: (response) => response.status(401).json({ error: 'token expired' }),
+  ER_BAD_FIELD_ERROR: (response) => response.status(400).json({ error: 'sql syntaxis error' })
 };
 
 // eslint-disable-next-line no-unused-vars
